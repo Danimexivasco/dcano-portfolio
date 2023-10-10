642061cb65e36e4ca07028d984c571b589d71983
Danimexivasco <dcano9493@gmail.com>
2023-10-10 22:03:21 +0200

    Folder restructure

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,15 @@
+import { Metadata } from "next"
 import styles from "./page.module.scss"
+
+
+export const metadata: Metadata = {
+  title: "Home",
+}
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Home
+      <h1>Home</h1>
     </main>
   )
 }
