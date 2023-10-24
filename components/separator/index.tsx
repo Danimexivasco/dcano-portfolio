a6889c00143ab6afa24975ebd57dc2cce18fe71b
Danimexivasco <dcano9493@gmail.com>
2023-10-24 10:45:35 +0200

    Create Separator component

---
+++ components/separator/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import styles from "./styles.module.scss"
+import { combine } from "@/helpers/combine";
+
+export interface SeparatorProps {
+  startPoint: "topLeft" | "bottomLeft",
+  filledPart: "top" | "bottom"
+}
+
+const Separator = ({ startPoint = "topLeft", filledPart }: SeparatorProps) => {
+  return (
+    <div className={combine(styles.separator, styles[ startPoint ], styles[ filledPart ])}></div>
+  )
+};
+
+
+export default Separator;
