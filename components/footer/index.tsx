638d75f785a65d4c0d63632593ba3f8f68fab77c
Danimexivasco <dcano9493@gmail.com>
2023-10-27 10:41:43 +0200

    Prototype Footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,14 +1,37 @@
 import React from "react"
 import styles from "./styles.module.scss"
 import Container from "../container";
+import Separator from "../separator";
+import Image from "next/image";
+import Link from "next/link";
+import { Dictionary } from "@/types";
+import Markdown from "react-markdown"
+import { SOCIALS } from "@/utils/constants";
+
+export interface FooterProps {
+  dictionary: Dictionary
+}
 
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <Container className={styles.footerContainer}>
-      <h2>Footer</h2>
-    </Container>
-  </footer>
+const Footer = ({ dictionary }: FooterProps) => (
+  <>
+    <Separator startPoint="bottomLeft" filledPart="bottom"/>
+    <footer className={styles.footer}>
+      <Container className={styles.footerContainer}>
+        <Markdown>{dictionary.footer.text}</Markdown>
+        <div className={styles.socials}>
+          {SOCIALS?.map(({ altText, icon, href }) => (
+            <Link key={altText} href={href}>
+              <Image
+                src={icon}
+                alt={altText}
+              />
+            </Link>
+          ))}
+        </div>
+      </Container>
+    </footer>
+  </>
 );
 
 
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,8 @@
+import LinkedinIcon from "/public/icons/linkedin.svg"
+import GithubIcon from "/public/icons/github.svg"
+import GitlabIcon from "/public/icons/gitlab.svg"
+import DevIcon from "/public/icons/dev.svg"
+
 export const LOCALES = [ "en-US", "es-ES" ]
 export const DEFAULT_LOCALE = "en-US"
 export const LOCALES_MAP = {
@@ -6,11 +11,34 @@ export const LOCALES_MAP = {
 }
 export const NAVIGATION = [
   {
-    slug: "/",
-    page: "home"
-  },
-  {
     slug: "/about",
     page: "about"
+  },
+  {
+    slug: "/projects",
+    page: "projects"
+  },
+]
+
+export const SOCIALS = [
+  {
+    altText: "linkedin-icon",
+    icon: LinkedinIcon,
+    href: "https://www.linkedin.com/in/daniel-cano-bernal-822b221a2/"
+  },
+  {
+    altText: "github-icon",
+    icon: GithubIcon,
+    href: "https://github.com/Danimexivasco"
+  },
+  {
+    altText: "gitlab-icon",
+    icon: GitlabIcon,
+    href: "https://gitlab.com/Danimexivasco"
+  },
+  {
+    altText: "dev-to-icon",
+    icon: DevIcon,
+    href: "https://dev.to/danimexivasco"
   },
 ]
