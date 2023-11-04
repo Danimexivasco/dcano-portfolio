9cf40257a2d8dba51b928e61301f9f4716cc994b
Danimexivasco <dcano9493@gmail.com>
2023-11-04 16:03:25 +0100

    Update icons, create an index and update constants

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,9 +1,6 @@
 import localFont from "next/font/local"
-import LinkedinIcon from "/public/icons/linkedin.svg"
-import GithubIcon from "/public/icons/github.svg"
-import GitlabIcon from "/public/icons/gitlab.svg"
-import DevIcon from "/public/icons/dev.svg"
 import { RandomColorMap } from "@/types"
+import { icons } from "./icons"
 
 export const LOCALES = [ "en-US", "es-ES" ]
 export const DEFAULT_LOCALE = "en-US"
@@ -25,22 +22,22 @@ export const NAVIGATION = [
 export const SOCIALS = [
   {
     altText: "linkedin-icon",
-    icon: LinkedinIcon,
+    icon: icons.LinkedinIcon,
     href: "https://www.linkedin.com/in/daniel-cano-bernal-822b221a2/"
   },
   {
     altText: "github-icon",
-    icon: GithubIcon,
+    icon: icons.GithubIcon,
     href: "https://github.com/Danimexivasco"
   },
   {
     altText: "gitlab-icon",
-    icon: GitlabIcon,
+    icon: icons.GitlabIcon,
     href: "https://gitlab.com/Danimexivasco"
   },
   {
     altText: "dev-to-icon",
-    icon: DevIcon,
+    icon: icons.DevIcon,
     href: "https://dev.to/danimexivasco"
   },
 ]
@@ -74,3 +71,213 @@ export const RANDOM_COLOR_MAP: RandomColorMap = {
   green: "81, 152, 114",
   red: "182, 72, 62"
 }
+
+export const TECHNOLOGIES = [
+  {
+    name: "React",
+    icon: icons.ReactIcon
+  },
+  {
+    name: "HTML 5",
+    icon: icons.Html5Icon
+  },
+  {
+    name: "CSS 3",
+    icon: icons.Css3Icon
+  },
+  {
+    name: "Javascript",
+    icon: icons.JsIcon
+  },
+  {
+    name: "Git",
+    icon: icons.GitIcon
+  },
+  {
+    name: "Api",
+    icon: icons.ApiIcon
+  },
+  {
+    name: "Cypress",
+    icon: icons.CypressIcon
+  },
+  {
+    name: "Gatsby",
+    icon: icons.GatsbyIcon
+  },
+  {
+    name: "Next.js",
+    icon: icons.NextIcon
+  },
+  {
+    name: "Sass",
+    icon: icons.SassIcon
+  },
+  {
+    name: "Typescript",
+    icon: icons.TsIcon
+  },
+  {
+    name: "Bootstrap",
+    icon: icons.BootstrapIcon
+  },
+  {
+    name: "Contentful",
+    icon: icons.ContentfulIcon
+  },
+  {
+    name: "Graphql",
+    icon: icons.GraphqlIcon
+  },
+  {
+    name: "Nodejs",
+    icon: icons.NodeJsIcon
+  },
+  {
+    name: "Express",
+    icon: icons.ExpressIcon
+  },
+  {
+    name: "Firebase",
+    icon: icons.FirebaseIcon
+  },
+  {
+    name: "GSAP",
+    icon: icons.GsapIcon
+  },
+  {
+    name: "Gulp",
+    icon: icons.GulpIcon
+  },
+  {
+    name: "Handlebars",
+    icon: icons.HandlebarsIcon
+  },
+  {
+    name: "jQuery",
+    icon: icons.JqueryIcon
+  },
+  {
+    name: "MongoDB",
+    icon: icons.MongodbIcon
+  },
+  {
+    name: "Redux",
+    icon: icons.ReduxIcon
+  },
+  {
+    name: "Strapi",
+    icon: icons.StrapiIcon
+  },
+  {
+    name: "Tests",
+    icon: icons.TestsIcon
+  },
+  {
+    name: "Zapier",
+    icon: icons.ZapierIcon
+  },
+  {
+    name: "Azure DevOps",
+    icon: icons.AzureIcon
+  },
+  {
+    name: "Styled Components",
+    icon: icons.StyledComponentsIcon
+  }
+]
+
+export const TIMELINE_ITEMS_EN = [
+  {
+    year: 2011,
+    headline: "Universidad Púbica del País Vasco (UPV / EHU)",
+    subHeadline: "Grado en Administración y Dirección de Empresas",
+    fromTo: "sept 2011 - may 2015",
+    duration: "(3 years and 9 months)"
+  },
+  {
+    year: 2014,
+    headline: "Universidad Modelo de Mérida, México",
+    subHeadline: "Erasmus +, End Of Degree Project",
+    fromTo: "aug 2014 - aug 2015",
+    duration: "(1 year)"
+  },
+  {
+    year: 2015,
+    headline: "Arpen SPR de RL de CV",
+    url: "https://arpen.mx/",
+    subHeadline: "Encargado del Departamento de Ventas (Stevia órganica)",
+    fromTo: "aug 2015 - apr 2016",
+    duration: "(9 months)"
+  },
+  {
+    year: 2018,
+    headline: "IES Plaiaundi BHI",
+    subHeadline: "Grado superior en desarrollo de aplicaciones web",
+    fromTo: "sept 2018 - may 2020",
+    duration: "(1 year and 9 months)"
+  },
+  {
+    year: 2019,
+    headline: "Lawyou",
+    url: "https://lawyoulegal.com/",
+    subHeadline: "Web Developer",
+    fromTo: "jun 2019 - jun 2020",
+    duration: "(1 year)"
+  },
+  {
+    year: 2020,
+    headline: "Digital Cuisine",
+    url: "https://www.cuisine.digital/",
+    subHeadline: "Front-end / Full-stack developer",
+    fromTo: "oct 2020 - now",
+    duration: "(3 years and 1 month)"
+  },
+]
+export const TIMELINE_ITEMS_ES = [
+  {
+    year: 2011,
+    headline: "Universidad Púbica del País Vasco (UPV / EHU)",
+    subHeadline: "Grado en Administración y Dirección de Empresas",
+    fromTo: "sept 2011 - may 2015",
+    duration: "(3 years and 9 months)"
+  },
+  {
+    year: 2014,
+    headline: "Universidad Modelo de Mérida, México",
+    subHeadline: "Erasmus +, End Of Degree Project",
+    fromTo: "aug 2014 - aug 2015",
+    duration: "(1 year)"
+  },
+  {
+    year: 2015,
+    headline: "Arpen SPR de RL de CV",
+    url: "https://arpen.mx/",
+    subHeadline: "Encargado del Departamento de Ventas (Stevia órganica)",
+    fromTo: "aug 2015 - apr 2016",
+    duration: "(9 months)"
+  },
+  {
+    year: 2018,
+    headline: "IES Plaiaundi BHI",
+    subHeadline: "Grado superior en desarrollo de aplicaciones web",
+    fromTo: "sept 2018 - may 2020",
+    duration: "(1 year and 9 months)"
+  },
+  {
+    year: 2019,
+    headline: "Lawyou",
+    url: "https://lawyoulegal.com/",
+    subHeadline: "Web Developer",
+    fromTo: "jun 2019 - jun 2020",
+    duration: "(1 year)"
+  },
+  {
+    year: 2020,
+    headline: "Digital Cuisine",
+    url: "https://www.cuisine.digital/",
+    subHeadline: "Front-end / Full-stack developer",
+    fromTo: "oct 2020 - now",
+    duration: "(3 years and 1 month)"
+  },
+]
---
+++ utils/icons.ts
@@ -0,0 +1,67 @@
+import LinkedinIcon from "/public/icons/linkedin.svg"
+import GithubIcon from "/public/icons/github.svg"
+import GitlabIcon from "/public/icons/gitlab.svg"
+import DevIcon from "/public/icons/dev.svg"
+import ApiIcon from "/public/icons/api.svg"
+import AzureIcon from "/public/icons/azure.svg"
+import BootstrapIcon from "/public/icons/bootstrap.svg"
+import ContentfulIcon from "/public/icons/contentful.svg"
+import Css3Icon from "/public/icons/css3.svg"
+import CypressIcon from "/public/icons/cypress.svg"
+import ExpressIcon from "/public/icons/express.svg"
+import FirebaseIcon from "/public/icons/firebase.svg"
+import GatsbyIcon from "/public/icons/gatsby.svg"
+import GitIcon from "/public/icons/git.svg"
+import GraphqlIcon from "/public/icons/graphql.svg"
+import GsapIcon from "/public/icons/gsap.svg"
+import GulpIcon from "/public/icons/gulp.svg"
+import HandlebarsIcon from "/public/icons/handlebars.svg"
+import Html5Icon from "/public/icons/html5.svg"
+import JqueryIcon from "/public/icons/jquery.svg"
+import JsIcon from "/public/icons/js.svg"
+import MongodbIcon from "/public/icons/mongodb.svg"
+import NextIcon from "/public/icons/next.svg"
+import NodeJsIcon from "/public/icons/nodejs.svg"
+import ReactIcon from "/public/icons/react.svg"
+import ReduxIcon from "/public/icons/redux.svg"
+import SassIcon from "/public/icons/sass.svg"
+import StrapiIcon from "/public/icons/strapi.svg"
+import StyledComponentsIcon from "/public/icons/styled_components.svg"
+import TestsIcon from "/public/icons/tests.svg"
+import TsIcon from "/public/icons/ts.svg"
+import ZapierIcon from "/public/icons/zapier.svg"
+
+export const icons = {
+  LinkedinIcon: LinkedinIcon,
+  GithubIcon: GithubIcon,
+  GitlabIcon: GitlabIcon,
+  DevIcon: DevIcon,
+  ApiIcon: ApiIcon,
+  AzureIcon: AzureIcon,
+  BootstrapIcon: BootstrapIcon,
+  ContentfulIcon: ContentfulIcon,
+  Css3Icon: Css3Icon,
+  CypressIcon: CypressIcon,
+  ExpressIcon: ExpressIcon,
+  FirebaseIcon: FirebaseIcon,
+  GatsbyIcon: GatsbyIcon,
+  GitIcon: GitIcon,
+  GraphqlIcon: GraphqlIcon,
+  GsapIcon: GsapIcon,
+  GulpIcon: GulpIcon,
+  HandlebarsIcon: HandlebarsIcon,
+  Html5Icon: Html5Icon,
+  JqueryIcon: JqueryIcon,
+  JsIcon: JsIcon,
+  MongodbIcon: MongodbIcon,
+  NextIcon: NextIcon,
+  NodeJsIcon: NodeJsIcon,
+  ReactIcon: ReactIcon,
+  ReduxIcon: ReduxIcon,
+  SassIcon: SassIcon,
+  StrapiIcon: StrapiIcon,
+  StyledComponentsIcon: StyledComponentsIcon,
+  TestsIcon: TestsIcon,
+  TsIcon: TsIcon,
+  ZapierIcon: ZapierIcon
+}
