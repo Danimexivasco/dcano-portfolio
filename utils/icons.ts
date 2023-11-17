6f86ee512a0ce6adf8ebaea9c1b06452e973be4b
Danimexivasco <dcano9493@gmail.com>
2023-11-17 12:48:04 +0100

    Update icons index file

--- utils/icons.ts
+++ utils/icons.ts
@@ -12,12 +12,13 @@ import ExpressIcon from "/public/icons/express.svg"
 import FirebaseIcon from "/public/icons/firebase.svg"
 import GatsbyIcon from "/public/icons/gatsby.svg"
 import GitIcon from "/public/icons/git.svg"
-import GraphqlIcon from "/public/icons/graphql.svg"
+import GraphQLIcon from "/public/icons/GraphQL.svg"
 import GsapIcon from "/public/icons/gsap.svg"
 import GulpIcon from "/public/icons/gulp.svg"
 import HandlebarsIcon from "/public/icons/handlebars.svg"
 import Html5Icon from "/public/icons/html5.svg"
 import JqueryIcon from "/public/icons/jquery.svg"
+import TailwindCssIcon from "/public/icons/tailwind.svg"
 import JsIcon from "/public/icons/js.svg"
 import MongodbIcon from "/public/icons/mongodb.svg"
 import NextIcon from "/public/icons/next.svg"
@@ -30,38 +31,71 @@ import StyledComponentsIcon from "/public/icons/styled_components.svg"
 import TestsIcon from "/public/icons/tests.svg"
 import TsIcon from "/public/icons/ts.svg"
 import ZapierIcon from "/public/icons/zapier.svg"
+import WebflowIcon from "/public/icons/webflow.svg"
+import CwaLogoIcon from "/public/icons/cwa_logo.svg"
+import IfLogoIcon from "/public/icons/if_logo.svg"
+import VimcarLogoIcon from "/public/icons/vimcar_logo.svg"
+import AvriosLogoIcon from "/public/icons/avrios_logo.svg"
+import FrientLogoIcon from "/public/icons/frient_logo.svg"
+import FrientLogoLightIcon from "/public/icons/frient_logo_light.svg"
+import PbfLogoIcon from "/public/icons/pbf_logo.svg"
+import PbfLogoLightIcon from "/public/icons/pbf_logo_light.svg"
+import SecovisaLogoIcon from "/public/icons/secovisa_logo.svg"
+import TeledataLogoIcon from "/public/icons/teledata_logo.svg"
+import TeledataLogoLightIcon from "/public/icons/teledata_logo_light.svg"
+import VierLogoIcon from "/public/icons/vier_logo.svg"
+import BahlsenLogoIcon from "/public/icons/bahlsen_logo.svg"
+import BahlsenLogoLightIcon from "/public/icons/bahlsen_logo_light.svg"
+import VetzLogoIcon from "/public/icons/vetz_logo.png"
 
 export const icons = {
-  LinkedinIcon: LinkedinIcon,
-  GithubIcon: GithubIcon,
-  GitlabIcon: GitlabIcon,
-  DevIcon: DevIcon,
-  ApiIcon: ApiIcon,
-  AzureIcon: AzureIcon,
-  BootstrapIcon: BootstrapIcon,
-  ContentfulIcon: ContentfulIcon,
-  Css3Icon: Css3Icon,
-  CypressIcon: CypressIcon,
-  ExpressIcon: ExpressIcon,
-  FirebaseIcon: FirebaseIcon,
-  GatsbyIcon: GatsbyIcon,
-  GitIcon: GitIcon,
-  GraphqlIcon: GraphqlIcon,
-  GsapIcon: GsapIcon,
-  GulpIcon: GulpIcon,
-  HandlebarsIcon: HandlebarsIcon,
-  Html5Icon: Html5Icon,
-  JqueryIcon: JqueryIcon,
-  JsIcon: JsIcon,
-  MongodbIcon: MongodbIcon,
-  NextIcon: NextIcon,
-  NodeJsIcon: NodeJsIcon,
-  ReactIcon: ReactIcon,
-  ReduxIcon: ReduxIcon,
-  SassIcon: SassIcon,
-  StrapiIcon: StrapiIcon,
-  StyledComponentsIcon: StyledComponentsIcon,
-  TestsIcon: TestsIcon,
-  TsIcon: TsIcon,
-  ZapierIcon: ZapierIcon
+  LinkedinIcon,
+  GithubIcon,
+  GitlabIcon,
+  DevIcon,
+  ApiIcon,
+  AzureIcon,
+  BootstrapIcon,
+  ContentfulIcon,
+  Css3Icon,
+  CypressIcon,
+  ExpressIcon,
+  FirebaseIcon,
+  GatsbyIcon,
+  GitIcon,
+  GraphQLIcon,
+  GsapIcon,
+  GulpIcon,
+  HandlebarsIcon,
+  Html5Icon,
+  JqueryIcon,
+  TailwindCssIcon,
+  JsIcon,
+  MongodbIcon,
+  NextIcon,
+  NodeJsIcon,
+  ReactIcon,
+  ReduxIcon,
+  SassIcon,
+  StrapiIcon,
+  StyledComponentsIcon,
+  TestsIcon,
+  TsIcon,
+  ZapierIcon,
+  WebflowIcon,
+  CwaLogoIcon,
+  IfLogoIcon,
+  VimcarLogoIcon,
+  AvriosLogoIcon,
+  FrientLogoIcon,
+  FrientLogoLightIcon,
+  PbfLogoIcon,
+  PbfLogoLightIcon,
+  SecovisaLogoIcon,
+  TeledataLogoIcon,
+  TeledataLogoLightIcon,
+  VierLogoIcon,
+  BahlsenLogoIcon,
+  BahlsenLogoLightIcon,
+  VetzLogoIcon
 }
