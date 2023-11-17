9dbbaf8b3cda11778e317ca8ab3cbe24223ab815
Danimexivasco <dcano9493@gmail.com>
2023-11-17 16:26:17 +0100

    Update Link component

--- components/link/index.tsx
+++ components/link/index.tsx
@@ -6,17 +6,32 @@ import { combine } from "@/helpers/combine";
 import styles from "./styles.module.scss"
 
 export interface LinkProps {
-  href: string,
-  asButton?: boolean,
-  className?: string,
-  onClick?: () => void,
+  href: string | undefined
+  asButton?: boolean
+  centered?: boolean
+  className?: string
+  external?: boolean
+  onClick?: () => void
   children: React.ReactNode
 }
 
 
-const Link = ({ href, asButton, onClick = () => {}, className, children }: LinkProps) => {
-  return (
-    <NextLink href={href} onClick={onClick} className={combine(styles.link, asButton && styles.asButton, className && className)}>
+const Link = ({ href, asButton, centered, external, onClick = () => {}, className, children }: LinkProps) => {
+  return external ? (
+    <a
+      href={href}
+      target="blank"
+      rel="noopener noreferrer"
+      className={combine(styles.link, asButton && styles.asButton, centered && styles.centered, className)}
+    >
+      {children}
+    </a>
+  ) : (
+    <NextLink
+      href={href ? href : "/"}
+      onClick={onClick}
+      className={combine(styles.link, asButton && styles.asButton, centered && styles.centered, className)}
+    >
       {children}
     </NextLink>
   )
