015ca888a5fd79d576dd8487d9db7893f4e8d68a
Danimexivasco <dcano9493@gmail.com>
2023-11-18 14:17:21 +0100

    Update timeline component

--- components/timeline/index.tsx
+++ components/timeline/index.tsx
@@ -1,19 +1,22 @@
 import React from "react"
+import ReactMarkdown from "react-markdown";
 import Container from "../container";
 import TimelineItem, { TimelineItemStructure } from "./item";
 import styles from "./styles.module.scss"
 
 
 export interface TimelineProps {
-  headline: string,
+  headline: string
+  text: string
   items: Array<TimelineItemStructure>
 }
 
-const Timeline = ({ headline, items = [] }: TimelineProps) => {
+const Timeline = ({ headline, text, items = [] }: TimelineProps) => {
   return (
     <section className={styles.timelineContainer}>
       <Container>
         <h2 className={styles.headline}>{headline}</h2>
+        <ReactMarkdown className={styles.text}>{text}</ReactMarkdown>
         <div className={styles.timeline}>
           {items.map((item, idx) => (
             <TimelineItem
--- components/timeline/item/index.tsx
+++ components/timeline/item/index.tsx
@@ -1,26 +1,26 @@
 "use client"
 
 import React from "react"
-import Link from "@/components/link";
-import { combine } from "@/helpers/combine";
 import { useInView } from "react-intersection-observer";
-import styles from "./styles.module.scss"
+import { combine } from "@/helpers/combine";
 import { scrollIntoView } from "@/helpers/scrollIntoView";
+import Link from "@/components/link";
+import styles from "./styles.module.scss"
 
 export type TimelineItemStructure = {
-  year: number;
-  headline: string;
-  subHeadline: string;
-  fromTo: string;
-  duration: string;
-  url?: undefined;
+  year: number
+  headline: string
+  subHeadline: string
+  fromTo: string
+  duration: string
+  url?: undefined
 } | {
-  year: number;
-  headline: string;
-  url: string;
-  subHeadline: string;
-  fromTo: string;
-  duration: string;
+  year: number
+  headline: string
+  url: string
+  subHeadline: string
+  fromTo: string
+  duration: string
 };
 
 export interface TimelineItemProps {
@@ -39,7 +39,7 @@ const TimelineItem = ({ item, containerClassName }: TimelineItemProps) => {
     <div className={combine(styles.container, containerClassName, inView && styles.active)} ref={ref} onClick={scrollIntoView}>
       <div className={styles.content}>
         <h4 className={styles.year}><b>{year}</b></h4>
-        <h4 className={combine(styles.headline, url && styles.link)}>{url ? <Link href={url}>{headline} &#10697;</Link>: headline}</h4>
+        <h4 className={combine(styles.headline, url && styles.link)}>{url ? <Link href={url} external>{headline} &#10697;</Link>: headline}</h4>
         <h5 className={styles.subHeadline}>{subHeadline}</h5>
         <p className={styles.fromTo}>{fromTo}</p>
         <small className={styles.duration}>{duration}</small>
