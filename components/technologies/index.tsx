2d4f04d3118eaa29c1ba94fd74e76e424b1d598a
Danimexivasco <dcano9493@gmail.com>
2023-11-18 14:17:10 +0100

    Update technologies component

--- components/technologies/index.tsx
+++ components/technologies/index.tsx
@@ -1,22 +1,24 @@
 "use client"
 
 import React, { useEffect } from "react"
-import styles from "./styles.module.scss"
-import Container from "../container";
-import Tag from "../tag";
-import { TechnologyItem } from "@/types";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ReactMarkdown from "react-markdown";
+import { TechnologyItem } from "@/types";
 import { combine } from "@/helpers/combine";
+import Container from "../container";
+import Tag from "../tag";
+import styles from "./styles.module.scss"
 gsap.registerPlugin(ScrollTrigger);
 
 export interface TechnologiesProps {
-  headline: string,
-  text: string,
+  headline?: string
+  text?: string
   items: Array<TechnologyItem>
+  className?: string
 }
 
-const Technologies = ({ headline, text, items }: TechnologiesProps) => {
+const Technologies = ({ headline, text, items, className }: TechnologiesProps) => {
 
   useEffect(() => {
     gsap.defaults({ ease: "power3" });
@@ -25,19 +27,24 @@ const Technologies = ({ headline, text, items }: TechnologiesProps) => {
       onEnter: batch => gsap.to(batch, { opacity: 1, y: 0, duration: 0.15, stagger: { each: 0.15, grid: [ 1, 3 ] }, overwrite: true }),
       onLeave: batch => gsap.set(batch, { opacity: 0, y: -70, duration: 0.15, overwrite: true }),
       onEnterBack: batch => gsap.to(batch, { opacity: 1, y: 0, duration: 0.15, stagger: 0.15, overwrite: true }),
-      onLeaveBack: batch => gsap.set(batch, { opacity: 0, y: 70, duration: 0.15, overwrite: true })
+      onLeaveBack: batch => gsap.set(batch, { opacity: 0, y: 70, duration: 0.15, overwrite: true }),
     });
 
     ScrollTrigger.addEventListener("refreshInit", () => {
       gsap.set(".tag", { y: 0 })
       return
     });
-  }, [])
+  }, [ items ])
+  
   return (
-    <section className={styles.technologies}>
+    <section className={combine(styles.technologies, className)}>
       <Container>
-        <h2 className={styles.title}>{headline}</h2>
-        <p>{text}</p>
+        {headline &&
+          <h2 className={styles.title}>{headline}</h2>
+        }
+        {text &&
+          <ReactMarkdown className={styles.text}>{text}</ReactMarkdown>
+        }
         <div className={styles.container}>
           {items?.map((item) => <Tag key={item.name} item={item} className={combine(styles.hidden, "tag")}/>)}
         </div>
